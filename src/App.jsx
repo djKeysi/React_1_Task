739dda207e67d4bb82c2eefe2efed2d85043cf11
Зенченко Евгений <zenchenko_es@outlook.com>
2025-02-14 22:37:57 +0700

    with jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const date = new Date()
 
   return (
     <>
@@ -18,18 +17,41 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+		<p>
+          {date.getFullYear()}
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
 
-export default App
+
+
+
+
+// import logo from './assets/react.svg';
+// import './App.css';
+
+// export const App = () => {
+// 	const date = new Date()
+// 	return   (
+// 		<div className="App">
+// 			<header className="App-header">
+// 				 <img src={logo} className="App-logo" alt="logo" />{/*ddfdf */}
+// 				<p></p>
+// 				<a
+// 					className="App-link"
+// 					href="https://reactjs.org"
+// 					target="_blank"
+// 					rel="noopener noreferrer"
+// 				>
+// 					Learn React
+// 				</a>
+// 				<p>
+//            {date.getFullYear()}
+//          </p>
+// 			</header>
+// 		</div>
+// 	);
+// };
+
