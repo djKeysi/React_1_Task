52a137d42ee49022043de76901fc57909d6f4269
Зенченко Евгений <zenchenko_es@outlook.com>
2025-02-14 23:17:31 +0700

    without jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
 
-export default function App() {
-  const date = new Date()
+// export default function App() {
+//   const date = new Date()
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-		<p>
-          {date.getFullYear()}
-        </p>
-      </div>
-    </>
-  )
-}
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vite.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+// 		<p>
+//           {date.getFullYear()}
+//         </p>
+//       </div>
+//     </>
+//   )
+// }
 
 
 
@@ -54,4 +54,30 @@ export default function App() {
 // 		</div>
 // 	);
 // };
+import './App.css';
+import { createElement } from 'react';
+import logo from './assets/react.svg';
+
+function createDate() {
+	const date = new Date()
+  return createElement(
+    'div',   // все что дальше в функции это императивный подход
+    { className: 'App' },
+
+    createElement('img',
+		{src : logo, className:'App-logo' },
+	),
+	createElement('p'),
+	createElement('a',
+			{href : 'https://reactjs.org', className:'App-link'},"Learn React"),
+			createElement('p',{},date.getFullYear()),
+  );
+}
+
+export default function App() { //декларативный подход
+  return createElement(
+    createDate
+
+  );
+}
 
